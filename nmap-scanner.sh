dcadf77a88a0b8c1fc66fc1533c3dac31256ec8c
SL <35158196+syncera@users.noreply.github.com>
2020-02-22 09:44:07 -0600

    Update nmap-scanner.sh

--- nmap-scanner.sh
+++ nmap-scanner.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-# This script is designed to scan the two domain controllers
-# and then the subnet using nmap and specified flags (if any)
+# This script is designed to scan two IPs 
+# and then their subnet using nmap and specified flags.
 # sincera
 
 weekday=$(date +%u)
 
 if [ $weekday--1 ]
 then
-  /usr/bin/nmap 10.0.13.254
+  /usr/bin/nmap 10.10.11.254
 elif [ $weekday==3 ]; then
-  /usr/bin/nmap 10.0.13.253
+  /usr/bin/nmap 10.10.11.253
 else
-  /usr/bin/nmap 10.0.12.0/23
+  /usr/bin/nmap 10.10.11.0/24
 fi
