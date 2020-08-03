c1af36419abced6906f98ed1cba60062640fb148
syncera <srlilly@pm.me>
2020-08-03 18:04:33 -0500

    streamline scraper

--- scraper.sh
+++ scraper.sh
@@ -2,19 +2,20 @@
 # This is a custom web scraper that will create an archive with a timestamp + MD5 hash. :)
 # sincera
 
-echo "Enter target website, including it's subdomain..."
-echo "Example: www.target.com"
-  read target
-echo "Scraping $target..."
+#echo "Enter target website, including it's subdomain..."
+#echo "Example: www.target.com"
+#  read target
+#  this has been removed to streamline the script.
+echo "Scraping $1..."
   prefix=$(date +'%m%d%Y')
-  wget -r -p -np -k $target
+  wget -r -p -np -k $1
 echo "Scrape complete. Archiving contents..."
-  tar -cf $prefix-$target.tar $target
-  gzip -v -9 -r -k *-$target.tar
+  tar -cf $prefix-$target.tar $1
+  gzip -v -9 -r -k *-$1.tar
 echo "Contents saved into current working directory."
 echo "Creating MD5 hash..."
-  md5 *.tar.gz >> $prefix-$target.md5
+  md5 *.tar.gz >> $prefix-$1.md5
 echo "Removing temporary files.."
   rm *.tar
-  rm -rf $target
-echo "Finished! $target has been scraped and archived with today's date."
+  rm -rf $1
+echo "Finished! $1 has been scraped and archived with today's date."
